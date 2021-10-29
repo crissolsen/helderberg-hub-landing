634dc82e0c14edd9a619a36b4baef846c1a72c8a
Christopher Olsen <olsen.criss@gmail.com>
2021-10-29 09:44:37 +0200

    Favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,16 +7,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'helderberg-hub-landing',
+    title: 'Helderberg Hubs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/png", href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌊</text></svg>" }],
+
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
